1891301090c0e1063de63dff83514876a8238e53
LikeAJohny <dev@johny.works>
2023-07-11 14:15:11 +0200

    add component for new session

---
+++ src/components/NewSession.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { Player, Variant } from '@/types/kniffel';
+	import { session } from '@stores/session.store';
+
+	export let player: Player;
+	export let variants: Variant[];
+
+	let variant: Variant = variants[0];
+	let numberOfGames: number = 1;
+
+	function handleSubmit(event: SubmitEvent) {
+		event.preventDefault();
+
+		session.destroy();
+		session.start(player, variant, numberOfGames);
+		goto('/play');
+	}
+</script>
+
+<form method="get" action="/play" on:submit={handleSubmit}>
+	<label for="variant">
+		<select name="variant" id="variant" bind:value={variant} required>
+			{#each variants as variant}
+				<option value={variant}>{variant.name}</option>
+			{/each}
+		</select>
+	</label>
+	<label for="number-of-games">
+		<input
+			type="number"
+			name="number-of-games"
+			id="number-of-games"
+			min="1"
+			max="1000"
+			placeholder="Number Of Games"
+			bind:value={numberOfGames}
+		/>
+	</label>
+	<button>Start</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,11 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import type { Variant } from '@/types/kniffel';
-	import { session } from '@stores/session.store';
+	import NewSession from '@components/NewSession.svelte';
 
 	export let data;
 	let { player, variants } = data;
 	$: ({ player, variants } = data);
-
-	let variant: Variant | '' = '';
-	let numberOfGames: number | '' = '';
-
-	function handleSubmit(event: SubmitEvent) {
-		event.preventDefault();
-
-		if (variant === '' || numberOfGames === '') {
-			alert('Please fill in all the fields');
-			return;
-		}
-
-		session.destroy();
-		session.start(player, variant, numberOfGames);
-		goto('/play');
-	}
 </script>
 
 <section class="new-game">
-	<form method="get" action="/play" on:submit={handleSubmit}>
-		<label for="variant">
-			<select name="variant" id="variant" bind:value={variant} required>
-				<option value="">Select A Variant</option>
-				{#each variants as variant}
-					<option value={variant}>{variant.name}</option>
-				{/each}
-			</select>
-		</label>
-		<label for="number-of-games">
-			<input
-				type="number"
-				name="number-of-games"
-				id="number-of-games"
-				min="1"
-				max="1000"
-				placeholder="Number Of Games"
-				bind:value={numberOfGames}
-			/>
-		</label>
-		<button>Start</button>
-	</form>
+	<NewSession {player} {variants} />
 </section>
